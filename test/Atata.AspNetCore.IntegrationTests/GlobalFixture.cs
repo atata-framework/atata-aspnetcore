a80f910fc3a532968d2aa038bf38ea05621314fd
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-31 15:33:28 +0100

    In integration tests projects extract NUnit attributes to NUnitAttributes.cs

--- test/Atata.AspNetCore.IntegrationTests/GlobalFixture.cs
+++ test/Atata.AspNetCore.IntegrationTests/GlobalFixture.cs
@@ -1,7 +1,4 @@
-﻿[assembly: SetCulture("en-US")]
-[assembly: Parallelizable(ParallelScope.Fixtures)]
-
-namespace Atata.AspNetCore.IntegrationTests;
+﻿namespace Atata.AspNetCore.IntegrationTests;
 
 public sealed class GlobalFixture : AtataGlobalFixture
 {

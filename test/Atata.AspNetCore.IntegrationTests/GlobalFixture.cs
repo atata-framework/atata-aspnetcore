0aa18ff4d5e8e781c08ac72b473818ef11500eb8
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-31 15:31:44 +0100

    Refactor `GlobalFixture`

--- test/Atata.AspNetCore.IntegrationTests/GlobalFixture.cs
+++ test/Atata.AspNetCore.IntegrationTests/GlobalFixture.cs
@@ -5,6 +5,4 @@ namespace Atata.AspNetCore.IntegrationTests;
 
 public sealed class GlobalFixture : AtataGlobalFixture
 {
-    protected override void ConfigureAtataContextGlobalProperties(AtataContextGlobalProperties globalProperties) =>
-        globalProperties.UseRootNamespaceOf<GlobalFixture>();
 }
